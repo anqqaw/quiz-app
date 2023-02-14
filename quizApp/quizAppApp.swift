d17a46800417936fd4f0ec6c468e452550f83ae9
Aarni Klemetti <aarni.klemetti@gmail.com>
2023-02-14 17:29:46 +0200

    Initial Commit

---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Aarni on 14.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
