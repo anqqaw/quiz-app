9baf448ea7132c0a2fa69d6f76c7226163162315
Aarni Klemetti <aarni.klemetti@gmail.com>
2023-04-11 18:24:05 +0300

    Changed JSON file and made flexible amount of answers and questions

--- quizApp/GameView.swift
+++ quizApp/GameView.swift
@@ -21,7 +21,6 @@ class QuestionNumber {
     func resetNumber(number: Int = 0) {
         questionNumber = number
     }
-
 }
 
 enum Option: CaseIterable {
@@ -39,7 +38,24 @@ enum Option: CaseIterable {
             return 3
         }
     }
+    
+    static func amount(sizeOfArray: Int) -> [Option] {
+        switch sizeOfArray {
+            case 1:
+                return [.first]
+            case 2:
+                return [.first, .second]
+            case 3:
+                return [.first, .second, .third]
+            case 4:
+                return [.first, .second, .third, .fourth]
+            default:
+                return []
+        }
+    }
 }
+
+
 
 struct GameView: View {
     @State private var lastQuestion = false
@@ -52,73 +68,74 @@ struct GameView: View {
 
 
     var body: some View {
-
         if lastQuestion {
             VictoryView(questionsCorrect: points)
         } else {
             ZStack(alignment: .top) {
                 Color(.blue)
                     .ignoresSafeArea()
-                VStack() {
-                    HStack() {
-                        Text("\(question.id)/4")
-                            .frame(width: 100, height: 100)
-                            .background(.orange)
-                    }
-
-                    Text("\(question.question)")
-                        .frame(width: 300, height: 100)
-                        .background(.yellow)
-                    HStack {
-                        RadioButtonGroup(selection: $selection,
-                                         orientation: .vertical,
-                                         tags: Option.allCases,
-                                         button: { isSelected in
-                            ZStack {
-                                Circle()
-                                    .foregroundColor(.purple)
-                                    .frame(width: 32, height: 32)
-                                if isSelected {
-                                    Circle()
-                                        .foregroundColor(Color.yellow)
-                                        .frame(width: 16, height: 16)
-                                }
-                            }
-                        },
-                                         label: { tag in
-                            Text(question.answers[(tag.description)])
-                        })
-                        Spacer()
-                    }
-                    .padding()
-                    .frame(width: 300, height: 400)
-                    .background(.green)
-                    
-                    Button() {
-                        if selection?.description == question.correct {
-                            points += 1
+                ScrollView(.vertical) {
+                    VStack() {
+                        HStack() {
+                            Text("\(questionNumber.getNumber() + 1)/\(ModelData().quizQuestions.count)")
+                                .frame(width: 100, height: 100)
+                                .background(.orange)
                         }
 
-                        if question.id == 4 {
-                            lastQuestion.toggle()
-                            questionNumber.resetNumber()
-                        }
-
-                        if question.id <= 3 {
-                            questionNumber.nextNumber()
-                            question = ModelData().quizQuestions[questionNumber.getNumber()]
-                        }
-
-                        selection = nil
-
-                    } label: {
-                        Text(question.id == 4 ? "End" : "Next")
+                        Text("\(question.question)")
                             .frame(width: 300, height: 100)
-                            .foregroundColor(.black)
-                            .background(.red)
+                            .background(.yellow)
+                        HStack {
+                            RadioButtonGroup(selection: $selection,
+                                             orientation: .vertical,
+                                             tags: Option.amount(sizeOfArray: question.answers.count),
+                                             button: { isSelected in
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(.purple)
+                                        .frame(width: 32, height: 32)
+                                    if isSelected {
+                                        Circle()
+                                            .foregroundColor(Color.yellow)
+                                            .frame(width: 16, height: 16)
+                                    }
+                                }
+                            },
+                                             label: { tag in
+                                Text(question.answers[(tag.description)])
+                            })
+                            Spacer()
+                        }
+                        .padding()
+                        .frame(width: 300, height: 250)
+                        .background(.green)
+
+                        Button() {
+                            if selection?.description == question.correct {
+                                points += 1
+                            }
+
+                            if questionNumber.getNumber() == ModelData().quizQuestions.count - 1 {
+                                lastQuestion.toggle()
+                                questionNumber.resetNumber()
+                            }
+
+                            if questionNumber.getNumber() < ModelData().quizQuestions.count - 1 {
+                                questionNumber.nextNumber()
+                                question = ModelData().quizQuestions[questionNumber.getNumber()]
+                            }
+
+                            selection = nil
+
+                        } label: {
+                            Text(questionNumber.getNumber() == ModelData().quizQuestions.count - 1 ? "End" : "Next")
+                                .frame(width: 300, height: 100)
+                                .foregroundColor(.black)
+                                .background(.red)
+                        }
                     }
+                    .padding(10)
                 }
-                .padding(10)
             }
             .navigationBarBackButtonHidden(true)
         }
